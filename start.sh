258e58fa8bef4550f1c6417ffba4ee61687cdfd0
furrieb-stack <y52s@yandex.com>
2026-07-11 20:30:02 +0300

    fix: remove --log-level error to see backend startup errors in Railway logs

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 sed -i "s/8080/${PORT:-8080}/g" /etc/nginx/nginx.conf
 
 cd /app/backend
-uvicorn main:app --host 0.0.0.0 --port 8000 --log-level error &
+uvicorn main:app --host 0.0.0.0 --port 8000 &
 PID_BACKEND=$!
 
 cd /app/frontend
